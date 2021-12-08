8a4b073f856fdccde2bccf8f2229360858c7bcbd
ebube <emmanuelsonzico@gmail.com>
2021-12-07 16:35:19 -0800

    getting data from database and displaying on dashboard

--- lib/widgets/item_list.dart
+++ lib/widgets/item_list.dart
@@ -1,4 +1,4 @@
- import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:myap/res/custom_colors.dart';
 import 'package:myap/screens/edit_screen.dart';
@@ -11,54 +11,112 @@ class ItemList extends StatelessWidget {
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
       stream: Database.readItems(),
-      builder: (context, snapshot) { 
+      builder: (context, snapshot) {
         if (snapshot.hasError) {
           return const Text('Something went wrong');
         } else if (snapshot.hasData || snapshot.data != null) {
-          return ListView.separated(
-            separatorBuilder: (context, index) => const SizedBox(height: 16.0),
-            itemCount: snapshot.data!.docs.length,
-            itemBuilder: (context, index) {
-              dynamic noteInfo = snapshot.data!.docs[index].data()!;
-              String docID = snapshot.data!.docs[index].id;
-              String title = noteInfo["title"];
-              String description = noteInfo["description"];
-              String location = noteInfo["location"];
-              String contact = noteInfo["contact"];
+          return Padding(
+            padding: const EdgeInsets.all(90.0),
+            child: ListView.separated(
+              separatorBuilder: (context, index) => const SizedBox(height: 16.0),
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                dynamic noteInfo = snapshot.data!.docs[index].data()!;
+                String docID = snapshot.data!.docs[index].id;
+                String title = noteInfo["title"];
+                String description = noteInfo["description"];
+                String location = noteInfo["location"];
+                String contact = noteInfo["contact"];
 
-              return Ink(
-                decoration: BoxDecoration(
-                  color: CustomColors.firebaseGrey.withOpacity(0.1),
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                child: ListTile(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  onTap: () => Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => EditScreen(
-                        currentTitle: title,
-                        currentDescription: description,
-                        currentLocation: location,
-                        currentContact: contact,
-                        documentId: docID,
-                      ),
+                return Ink(
+                    decoration: BoxDecoration(
+                      color: CustomColors.firebaseGrey.withOpacity(0.1),
+                      borderRadius: BorderRadius.circular(8.0),
                     ),
-                  ),
-                  title: Text(
-                    title,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  subtitle: Text(
-                    description,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-              );
-            },
+                    child: GestureDetector(
+                      onTap: () => Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => EditScreen(
+                            currentTitle: title,
+                            currentDescription: description,
+                            currentLocation: location,
+                            currentContact: contact,
+                            documentId: docID,
+                          ),
+                        ),
+                      ),
+                      child: 
+                       Card(
+                          color: CustomColors.firebaseNavy,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          child: 
+                            Column(
+                              children: [
+                                const SizedBox(
+                                  height: 10.0,
+                                ),
+                                Text(
+                                  title,
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                                const SizedBox(
+                                  height: 20.0,
+                                ),
+                                Text(
+                                  description,
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                                const SizedBox(
+                                  height: 10.0,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      Column(
+                                        children: [
+                                          const Padding(
+                                            padding: EdgeInsets.only(top: 4.0),
+                                            child: Text(
+                                              "Location  ",
+                                              style: TextStyle(fontSize: 8),
+                                            ),
+                                          ),
+                                          Text(
+                                            location,
+                                            maxLines: 1,
+                                            overflow: TextOverflow.ellipsis,
+                                          ),
+                                        ],
+                                      ),
+                                      Column(
+                                        children: [
+                                          const Padding(padding: EdgeInsets.only(top: 4.0),
+                                          child: Text("Contact  ", 
+                                          style: TextStyle(fontSize: 8),),
+                                          ),
+                                          Text(
+                                            contact,
+                                            maxLines: 2,
+                                            overflow: TextOverflow.fade,
+                                            softWrap: false,
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                )));
+              },
+            ),
           );
         }
 
@@ -73,3 +131,31 @@ class ItemList extends StatelessWidget {
     );
   }
 }
+
+
+// ListTile(
+//     shape: RoundedRectangleBorder(
+//       borderRadius: BorderRadius.circular(8.0),
+//     ),
+//     onTap: () => Navigator.of(context).push(
+//       MaterialPageRoute(
+//         builder: (context) => EditScreen(
+//           currentTitle: title,
+//           currentDescription: description,
+//           currentLocation: location,
+//           currentContact: contact,
+//           documentId: docID,
+//         ),
+//       ),
+//     ),
+//     title: Text(
+//       title,
+//       maxLines: 1,
+//       overflow: TextOverflow.ellipsis,
+//     ),
+//     subtitle: Text(
+//       description,
+//       maxLines: 1,
+//       overflow: TextOverflow.ellipsis,
+//     ),
+//   ),
